bd8b7c81c890fcbdb6d44bdbb7d24dde580baebe
Matt <m.b.smith-5@umcutrecht.nl>
2023-12-13 10:14:48 +0100

    added a script to start a prediction remotely.

--- request_prediction.sh
+++ request_prediction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 image=$1
-host=localhost
-#starts a server that will make predictions using the provided model
+host=$2
+#requests a prediction from a running server
 mvn exec:java -D"exec.mainClass"="org.orangepalantir.PredictionClient" -Dexec.args="$image $host"
 
