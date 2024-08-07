8c6ded641b0da5c61cecb3d05e9bc4b4d660a671
Matt <m.b.smith-5@umcutrecht.nl>
2024-08-07 13:41:25 +0200

    Upgraded to version 1.0.0-rc.1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module tfII {
     exports org.orangepalantir;
     requires ij;
-    requires org.tensorflow;
+    requires tensorflow;
     requires org.tensorflow.ndarray;
     requires java.desktop;
 }
---
+++ src/test/java/org/orangepalantir/NormalizationTest.java
@@ -0,0 +1,2 @@
+package org.orangepalantir;public class NormalizationTest {
+}
